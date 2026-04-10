83fdacfe023785bda76429a8d5b1f0ea1bfc209a
Gregg <74693768+gjrsas@users.noreply.github.com>
2026-04-10 12:26:12 -0400

    Add CVSS and KEV models to __all__ exports
    Expanded the __all__ export list to include additional model classes.

--- src/mcp_nvd_server/models/__init__.py
+++ src/mcp_nvd_server/models/__init__.py
@@ -1,3 +1,26 @@
 from mcp_nvd_server.models.cve import CVESearchResult, CVESummary
+from mcp_nvd_server.models.cvss import 
+(
+  CVSSInterpretation,
+  CVSSMetricValue,
+  CVSSScore,
+  NormalizedCVE,
+  ParsedCVSSVector,
+)
+from mcp_nvd_server.models.history import CVEHistoryChange, CVEHistoryResult
+from mcp_nvd_server.models.kev import CisaKevMetadata, KEVLookupResult, KEVRecord
 
-__all__ = ["CVESearchResult", "CVESummary"]
+__all__ = [
+  "CVESearchResult", 
+  "CVESummary",
+  "CVSSInterpretation",
+  "CVSSMetricValue",
+  "CVSSScore",
+  "NormalizedCVE",
+  "ParsedCVSSVector",
+  "CVEHistoryChange", 
+  "CVEHistoryResult",
+  "CisaKevMetadata", 
+  "KEVLookupResult", 
+  "KEVRecord",
+  ]
